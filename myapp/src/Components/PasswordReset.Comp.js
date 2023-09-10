0c8e67f851f47a32440ae93e3313b24e416dbb43
Appu9895 <afnanhudli2@gmail.com>
2023-09-09 19:10:24 -0700

    PasswordFile Added

--- myapp/src/Components/PasswordReset.Comp.js
+++ myapp/src/Components/PasswordReset.Comp.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Container,Row,Col,Form, Button} from "react-bootstrap"
-export const LoginForm = ({onChangeEvent,email,pass,SubmitData}) => {
+export const PasswordReset = ({onChangeEvent,email,pass,SubmitData}) => {
   return (
     <Container>
         <Row>
             <Col>
-            <h1 className="text-info text-center">Client Login</h1>
+            <h1 className="text-info text-center">Reset Password</h1>
             <hr/>
             <Form  autoComplete='off'>
                 <Form.Group>
@@ -14,23 +14,14 @@ export const LoginForm = ({onChangeEvent,email,pass,SubmitData}) => {
                         Email Address
                     </Form.Label>
                     <Form.Control onChange={onChangeEvent} value={email} type="email" name="email" placeholder='Enter Email ' required />
-                    <Form.Label>
-                        Password
-                    </Form.Label>
-                    <Form.Control onChange={onChangeEvent} value={pass} type="password" name="password" placeholder='Enter Password' required />
+                   
                 </Form.Group>
                 <Button className='form-box'onClick={SubmitData} type="submit">Login</Button>
             </Form>
 
             </Col>
         </Row>
-        <hr/>
-        <Row>
-            <Col>
-            <a href="#">Forget Password</a>
-            </Col>
-        </Row>
-
+     
     </Container>
   )
 }
--- myapp/src/Pages/EntryPage/Entry.js
+++ myapp/src/Pages/EntryPage/Entry.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Entry.css"
 import { LoginForm } from '../../Components/Login/Login.Comp';
+import { PasswordReset } from '../../Components/PasswordReset.Comp';
 
 import Jumbotron from 'react-bootstrap/Jumbotron'
 export const Entry = () => {
+  const [email,setEmail]=useState();
+  const [password,setPassword]=useState();
+  Const [typeImplementation,settype]=useState("Login");
+  
+  const onChangeEvent=(e)=>{
+      const {name, value}=e.target;
+      switch(name){
+        case 'email': setEmail(value);
+        break;
+        case 'password': setPassword(value);
+        break;
+        default:
+        break;
+
+      }
+  }
+
+  const SubmitData=(e)=>{
+  
+    console.log("button invoked..");
+    if(!email || !password){
+      alert("Please fill all details....!!!!");
+    }
+  }
   return (
 <div className="entry-page bg-info">
-<Jumbotron>
-    <LoginForm/>
+<Jumbotron className='form-box'>
+    <LoginForm onChangeEvent={onChangeEvent} email={email} pass={password} SubmitData={SubmitData} />
+    <PasswordReset onChangeEvent={onChangeEvent} email={email}  SubmitData={SubmitData} />
+
 </Jumbotron>
 </div>  )
 }
